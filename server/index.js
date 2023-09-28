8214310a121be799b82062d77478f2666db6fc2b
Avinash Kotyada <56509583+avinashkotyada@users.noreply.github.com>
2023-09-28 10:15:01 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 import dotenv from 'dotenv'
 dotenv.config()
 import mongoose from "mongoose";
@@ -177,20 +177,18 @@ app.post('/api/message', async (req, res) => {
     try {
         const { conversationId, senderId, message, receiverId=''} = req.body;
         if(!senderId || !message) return res.status(400).send('Please fill all the required fields');
-        if(!conversationId=='new' && receiverId ){
+        if(conversationId=='new' && receiverId ){
             const newConversation= new Conversations({members:[senderId,receiverId]});
             await newConversation.save();
-            const newMessage=new Messages({conversationId:newConversation._id,senderId,message,receiverId});
+            console.log(newConversation);
+            const newMessage=new Messages({conversationId:newConversation._id,senderId,message});
+            await newMessage.save();
+            return res.status(200).send('Message sent successfully');
+        }else{
+            const newMessage = new Messages({ conversationId, senderId, message });
             await newMessage.save();
             return res.status(200).send('Message sent successfully');
         }
-        else if(!conversationId && !receiverId){
-            return res.status(400).send('Please fill all the required fields');
-        }
-       
-        const newMessage = new Messages({ conversationId, senderId, message });
-        await newMessage.save();
-        res.status(200).send('Message sent successfully');
     } catch (error) {
         console.log(error, 'Error')
     }
